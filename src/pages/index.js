d2f5232ce98c0d9198467daf484f8d23e4510bb5
Dino Bulic <dinobulic@urus.local>
2019-04-09 15:06:51 +0200

    add bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,13 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h3>Hello from Gatsby!</h3>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">TEST</div>
+        <div className="col-5">TEST</div>
+        <div className="col-1">TEST</div>
+      </div>
+    </div>
   </Layout>
 )
 
