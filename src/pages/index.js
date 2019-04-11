aded78440aceb2bc3491f118514f2381292118b8
Dino Bulic <dinobulic@urus.local>
2019-04-11 11:52:42 +0200

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FaJediOrder } from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hello from Gatsby!</h1>
+    <FaJediOrder />
   </Layout>
 )
 
