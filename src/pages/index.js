379e3f435795ab24e1bc7e3ad2ce9759837129eb
Dino Bulic <dinobulic@urus.local>
2019-04-11 11:15:28 +0200

    spring cleaning for coffe shop setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
-//import { Link } from "gatsby"
-import { FaBeer } from "react-icons/fa"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,18 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>Hello from Gatsby!</h3>
-    <div className="container">
-      <h3>
-        Lets go for a <FaBeer />?{" "}
-      </h3>
-      <div className="row">
-        <div className="col-6">TEST</div>
-
-        <div className="col-5">TEST</div>
-        <div className="col-1">TEST</div>
-      </div>
-    </div>
+    <h1>Hello from Gatsby!</h1>
   </Layout>
 )
 
