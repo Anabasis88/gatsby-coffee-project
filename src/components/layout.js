59bf9c57d995f711a782453e41c0e021bc57ec76
Dino Bulic <dinobulic@urus.local>
2019-04-11 15:17:17 +0200

    add staticquerry

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,12 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
 
 import "./bootstrap.min.css"
 import "./layout.css"
 
+import Navbar from "./Globals/Navbar"
 const Layout = ({ children }) => {
   return <React.Fragment>{children}</React.Fragment>
 }
