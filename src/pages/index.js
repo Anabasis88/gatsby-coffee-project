b580e808995574472867f6e0e4d3f800462616ec
Dino Bulic <dinobulic@urus.local>
2019-04-11 09:58:19 +0200

    page setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 //import { Link } from "gatsby"
+import { FaBeer } from "react-icons/fa"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,8 +10,12 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h3>Hello from Gatsby!</h3>
     <div className="container">
+      <h3>
+        Lets go for a <FaBeer />?{" "}
+      </h3>
       <div className="row">
         <div className="col-6">TEST</div>
+
         <div className="col-5">TEST</div>
         <div className="col-1">TEST</div>
       </div>
